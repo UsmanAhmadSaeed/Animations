853a5d319847cd77d2d3f078d1d76dbab68cf874
Anupriya Jain <86339110+anupriyajain595@users.noreply.github.com>
2022-10-17 20:27:45 +0530

    Hey! @UsmanAhmadSaeed , I've raised PR  #40 for the issue. Could you please review it?

---
+++ Typing Animation/script.js
@@ -0,0 +1,7 @@
+//typing animation script
+var typed = new Typed(".typing", {
+    strings: ["Text-Typing-Animation.","CSS-Text-Typing.","Animations."],
+   typeSpeed: 100,
+   backSpeed: 60,
+   loop: true
+});
